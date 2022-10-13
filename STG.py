bdef91445a61535b605f4ed6952f06d954d5494a
unknown <daniellopez960814@gmail.com>
2022-10-13 16:42:59 -0500

    UPDATE

---
+++ STG.py
@@ -0,0 +1,303 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import OLSInfluence
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_poisson_deviance
+from sklearn.metrics import mean_gamma_deviance
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import StackingRegressor
+import matplotlib.pyplot as plt
+
+#MAE por debajo de 0.05
+class ExperimentData:
+    def apply(self, data_raw, output_feature, sample=1):
+        metadataExperiment = {}
+        if sample != 1:
+            data = data_raw.sample(frac=sample, random_state=17)
+        else:
+            data = data_raw
+        X = data.drop(output_feature, axis=1)
+        Y = data[output_feature]
+        X_Norm_Train, X_Norm_Test, Y_Train, Y_Test = train_test_split(
+            X, Y, test_size=0.25, train_size=0.75, random_state=17
+        )
+        metadataExperiment["input_features_train"] = X_Norm_Train
+        metadataExperiment["input_features_test"] = X_Norm_Test
+        metadataExperiment["output_feature_train"] = Y_Train
+        metadataExperiment["output_feature_test"] = Y_Test
+        return metadataExperiment
+
+class Metrics_Regression:
+    def preprocess_output(self, output_feature_test, predicted_output_feature):
+        MN = MinMaxScaler(feature_range=(0, 1))
+        Y_Test_R = output_feature_test.values
+        Y_Test_R = np.asarray(Y_Test_R)
+        Y_merge = np.vstack([Y_Test_R, predicted_output_feature]).T
+        for _, n in enumerate(np.arange(0, Y_merge.shape[0])):
+            if Y_merge[n, 0] == 0:
+                Y_merge[n, 0] = 0.0001
+            elif Y_merge[n, 1] == 0:
+                Y_merge[n, 1] = 0.0001
+        return Y_merge
+    def calculate_metric(self, LR, input_features_test, output_feature_test, metric):
+        try:
+            if metric == "explained_variance_score":
+                predicted_output_feature = LR.predict(input_features_test)
+                result = explained_variance_score(
+                    output_feature_test, predicted_output_feature
+                )
+            elif metric == "mean_absolute_error":
+                predicted_output_feature = LR.predict(input_features_test)
+                result = mean_absolute_error(output_feature_test, predicted_output_feature)
+            elif metric == "mean_poisson_deviance":
+                predicted_output_feature = LR.predict(input_features_test)
+                preprocess_output_ = self.preprocess_output(
+                    output_feature_test, predicted_output_feature
+                )
+                result = mean_poisson_deviance(
+                    preprocess_output_[:, 0], preprocess_output_[:, 1]
+                )
+            elif metric == "mean_gamma_deviance":
+                predicted_output_feature = LR.predict(input_features_test)
+                preprocess_output_ = self.preprocess_output(
+                    output_feature_test, predicted_output_feature
+                )
+                result = mean_gamma_deviance(
+                    preprocess_output_[:, 0], preprocess_output_[:, 1]
+                )
+            else:
+                result = cross_val_score(
+                    LR, input_features_test, output_feature_test, cv=5, scoring=metric
+                ).mean()
+        
+            return result
+        except:
+            return np.nan
+        
+    def getMetricDataFrame(
+        self,
+        description,
+        mse,
+        r2,
+        explained_variance_score_,
+        mean_absolute_error_,
+        mean_poisson_deviance_,
+        mean_gamma_deviance_,
+        is_linear_reg=False
+    ):
+        if is_linear_reg:
+            record = [
+                description,
+                mse,
+                r2,
+                explained_variance_score_,
+                mean_absolute_error_,
+                mean_poisson_deviance_,
+                mean_gamma_deviance_
+            ]
+        else:
+            record = [
+                description,
+                mse,
+                np.nan,
+                explained_variance_score_,
+                mean_absolute_error_,
+                mean_poisson_deviance_,
+                mean_gamma_deviance_
+            ]
+
+        columnsNames = [
+            "description",
+            "mse",
+            "r2",
+            "explained_variance_score_",
+            "mean_absolute_error_",
+            "mean_poisson_deviance_",
+            "mean_gamma_deviance_"
+        ]
+        df = pd.DataFrame(data=[record], columns=columnsNames)
+        return df
+    def get_model_metadata(self, LR, input_features_test, output_feature_test, description, is_linear_regression=False):
+        mse = self.calculate_metric(
+            LR,
+            input_features_test,
+            output_feature_test,
+            metric="neg_mean_squared_error",
+            )
+        r2 = self.calculate_metric(LR, input_features_test, output_feature_test, metric="r2")
+        explained_variance_score_ = self.calculate_metric(
+            LR,
+            input_features_test,
+            output_feature_test,
+            metric="explained_variance_score",
+            )
+        mean_absolute_error_ = self.calculate_metric(
+            LR, input_features_test, output_feature_test, metric="mean_absolute_error"
+            )
+        mean_poisson_deviance_ = self.calculate_metric(
+            LR, input_features_test, output_feature_test, metric="mean_poisson_deviance"
+            )
+        mean_gamma_deviance_ = self.calculate_metric(
+            LR, input_features_test, output_feature_test, metric="mean_gamma_deviance"
+            )
+        df = self.getMetricDataFrame(
+                description,
+                mse,
+                r2,
+                explained_variance_score_,
+                mean_absolute_error_,
+                mean_poisson_deviance_,
+                mean_gamma_deviance_,
+                is_linear_regression
+            )
+        return df
+
+class LinearRegressionPreprocessing:
+    def get_influencias(self, data, output_feature):
+        input_features, output_features = data.drop(output_feature, axis=1), data[output_feature]
+        output_features_R = np.asarray(output_features.values)
+        transformer = sm.OLS(output_features_R, input_features.assign(const=1))
+        output_data_raw = transformer.fit()
+        output_data_process = OLSInfluence(output_data_raw)
+        th = 0.9*output_data_process.hat_matrix_diag.max()
+        # print(th)
+        # fig, ax = plt.subplots(figsize=(8, 8))
+        # ax.scatter(
+        #         output_data_process.hat_matrix_diag, 
+        #         output_data_process.resid_studentized_internal, 
+        #         s=1000 * np.sqrt(output_data_process.cooks_distance[0]),
+        #         alpha=0.7
+        #     )
+        # plt.show()
+        output_data_process=output_data_process.cooks_distance[0]
+        samples_filtered = [distance < th for distance in output_data_process]
+        input_features_processed = input_features.loc[samples_filtered]
+        output_features_processed = pd.DataFrame(output_features_R, columns=[output_feature]).loc[
+            samples_filtered
+        ]
+        return pd.concat(
+            [
+                input_features_processed, 
+                output_features_processed
+            ], 
+            axis=1).reset_index().drop("index", axis=1)
+
+class RawLinearRegression(Metrics_Regression):
+    def trainLinearRegression(self, input_features_train, output_feature_train):
+        LR = LinearRegression()
+        LR.fit(input_features_train, output_feature_train)
+        return LR
+
+    def get_feature_weights(self, LR, input_features_train):
+        score_df = pd.DataFrame(
+            LR.coef_, index=input_features_train.columns, columns=["Score"]
+        )
+        return score_df
+
+    def apply(self, data_raw, output_feature, description, sample=1):
+        metadata = {}
+        metadataExperiment = ExperimentData().apply(data_raw, output_feature, sample)
+        LR = self.trainLinearRegression(
+            metadataExperiment["input_features_train"],
+            metadataExperiment["output_feature_train"]
+            )
+        metric_information = self.get_model_metadata(
+                LR, 
+                metadataExperiment["input_features_test"], 
+                metadataExperiment["output_feature_test"], 
+                description,
+                is_linear_regression=True
+            )
+        metadata["regressor"] = LR 
+        metadata["metric_information"] = metric_information
+        return metadata
+
+class RobustLinearRegression(Metrics_Regression):
+    def trainRobustLinearRegression(
+        self,
+        input_features_train,
+        output_feature_train,
+        max_trials=1000,
+        residual_threshold=0.2,
+        stop_probability=0.99,
+    ):
+        LR = LinearRegression()
+        LR.fit(input_features_train, output_feature_train)
+        MR = RANSACRegressor(
+            base_estimator=LR,
+            max_trials=max_trials,
+            residual_threshold=residual_threshold,
+            stop_probability=stop_probability,
+        )
+        MR.fit(input_features_train, output_feature_train)
+        return MR
+
+    def apply(self, data_raw, output_feature, description, sample=1):
+        metadata = {}
+        metadataExperiment = ExperimentData().apply(data_raw, output_feature, sample)
+        LR = self.trainRobustLinearRegression(
+            metadataExperiment["input_features_train"],
+            metadataExperiment["output_feature_train"]
+            )
+        metric_information = self.get_model_metadata(
+                LR, 
+                metadataExperiment["input_features_test"], 
+                metadataExperiment["output_feature_test"], 
+                description
+            )
+        metadata["regressor"] = LR 
+        metadata["metric_information"] = metric_information
+        return metadata
+
+class stackingRegressor(Metrics_Regression):
+    def trainRobustLinearRegression(self, input_features_train, output_feature_train):
+        RF_1 = RandomForestRegressor(
+            n_estimators=200,
+            criterion="absolute_error",
+            max_depth=7,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            bootstrap=True,
+            n_jobs=-1,
+            ccp_alpha=0.0,
+        )
+        RF_2 = RandomForestRegressor(
+            n_estimators=150,
+            criterion="squared_error",
+            max_depth=10,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            bootstrap=True,
+            n_jobs=-1,
+            ccp_alpha=0.0,
+        )
+        LR = LinearRegression()
+        models = {("Random Forest 1", RF_1), ("Random Forest 2", RF_2)}
+        SCR = StackingRegressor(models, final_estimator=LR, cv=2, n_jobs=-1)
+        SCR.fit(input_features_train, output_feature_train)
+        return SCR
+
+    def apply(self, data_raw, output_feature, description, sample=1):
+        metadata = {}
+        metadataExperiment = ExperimentData().apply(data_raw, output_feature, sample)
+        LR = self.trainRobustLinearRegression(
+                metadataExperiment["input_features_train"],
+                metadataExperiment["output_feature_train"]
+            )
+        metric_information = self.get_model_metadata(
+                LR, 
+                metadataExperiment["input_features_test"], 
+                metadataExperiment["output_feature_test"], 
+                description
+            )
+        metadata["regressor"] = LR 
+        metadata["metric_information"] = metric_information
+        return metadata
+
---
+++ Script.py
@@ -0,0 +1,252 @@
+from public.categoricalPreprocessing import *
+from public.kernelPCA import *	 
+from public.spearmanCorrelation import *
+from public.common import *		       
+from public.Kmeans import *	     
+from public.statistic import *
+from public.dimensionalReductionCommon import *  
+from public.pca import *
+from public.ica import *			       
+from public.removeOutliersLOF import *
+from pandas.api.types import is_numeric_dtype
+import pandas as pd
+import pickle
+
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import OLSInfluence
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_poisson_deviance
+from sklearn.metrics import mean_gamma_deviance
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RANSACRegressor
+from sklearn.model_selection import train_test_split
+
+from STG import *
+
+
+def imputation_data(data_processed):
+  columns = data_processed.columns
+  for column in columns:
+    median = data_processed[column].median()
+    data_processed[column] = data_processed[column].fillna(median)
+  return data_processed
+
+def executeOutlierAnalisis(CHECKPOINT_LOF):
+    if not CHECKPOINT_LOF:
+        df = pd.read_csv('household_power_consumption.txt', sep=';')
+        df_raw = df.replace("?", np.nan)
+        data_transformed = df_raw.copy()
+        data_transformed["Date"] = (
+            data_transformed["Date"]
+            .apply(lambda x: x.split("/"))
+            .apply(lambda x: "{}{}{}".format(x[2], x[1], x[0]))
+            .astype(np.int)
+        )
+        data_transformed["Time"] = (
+            data_transformed["Time"]
+            .apply(lambda x: x.split(":"))
+            .apply(lambda x: int(x[0])*60*60 + int(x[1])*60 + int(x[2]))
+        )
+        data_transformed = data_transformed.astype(float)
+        data_imputed = imputation_data(data_transformed)
+        output_feature = "Global_intensity"
+        summary_df, data_dic_collection = (
+            Remove_Outliers_LOF()
+            .process_outliers_non_labeled(
+                    data_imputed, 
+                    regression=True, 
+                    feature=output_feature
+                )
+            )
+        
+        with open('data_dic_collection.pickle', 'wb') as f:
+            pickle.dump(data_dic_collection, f)
+        f.close()
+        with open('data_imputed.pickle', 'wb') as f:
+            pickle.dump(data_imputed, f)
+        f.close()
+    else:    
+        file = open('data_dic_collection.pickle', 'rb')
+        data_dic_collection = pickle.load(file)
+        file.close()
+        file = open('data_imputed.pickle', 'rb')
+        data_imputed = pickle.load(file)
+        file.close()
+    return data_imputed, data_dic_collection
+
+def executeOutlierAnalisisWithoutTime(CHECKPOINT_LOF):
+    if not CHECKPOINT_LOF:
+        df = pd.read_csv('household_power_consumption.txt', sep=';')
+        df_raw = df.replace("?", np.nan)
+        data_transformed = df_raw.copy()
+        data_transformed = data_transformed.drop("Date", axis=1).drop("Time", axis=1)
+        data_transformed = data_transformed.astype(float)
+        data_imputed = imputation_data(data_transformed)
+        output_feature = "Global_intensity"
+        summary_df, data_dic_collection = (
+            Remove_Outliers_LOF()
+            .process_outliers_non_labeled(
+                    data_imputed, 
+                    regression=True, 
+                    feature=output_feature
+                )
+            )
+        
+        with open('data_dic_collection_without_time.pickle', 'wb') as f:
+            pickle.dump(data_dic_collection, f)
+        f.close()
+        with open('data_imputed_without_time.pickle', 'wb') as f:
+            pickle.dump(data_imputed, f)
+        f.close()
+    else:    
+        file = open('data_dic_collection_without_time.pickle', 'rb')
+        data_dic_collection = pickle.load(file)
+        file.close()
+        file = open('data_imputed_without_time.pickle', 'rb')
+        data_imputed = pickle.load(file)
+        file.close()
+    return data_imputed, data_dic_collection
+
+def getDicScalerData(data_imputed, data_dic_collection, output_feature = "Global_intensity"):
+    data_dic = {}
+    data_dic[""] = get_scale_data_regression(
+        data_imputed, 
+        data_dic_collection, 
+        scaler="", 
+        feature=output_feature
+        )
+    data_dic["robust"] = get_scale_data_regression(
+        data_imputed, 
+        data_dic_collection, 
+        scaler="robust", 
+        feature=output_feature
+        )
+    data_dic["StandardScaler"] = get_scale_data_regression(
+        data_imputed, 
+        data_dic_collection, 
+        scaler="StandardScaler", 
+        feature=output_feature
+        )
+    data_dic["mixMax"] = get_scale_data_regression(
+        data_imputed, 
+        data_dic_collection, 
+        scaler="mixMax", 
+        feature=output_feature
+        )
+    return data_dic
+
+def get_summary_algorithms(data_dic, scaler, flag_first=True, summary_dim_reduction=None, regression=False, output=None):
+  algoritms = [PCA_(),
+               ICA_()]
+  for algorimth in algoritms:
+    data = data_dic[scaler]
+    if regression:
+      X, Y = data.drop(output, axis=1), data[output]
+      algorimth.apply(X, scaler)
+    else:
+      algorimth.apply(data, scaler)
+    if flag_first:
+      summary_dim_reduction = algorimth.metadata["summary"]
+      flag_first = False
+    else:
+      df = algorimth.metadata["summary"]
+      summary_dim_reduction = concat_vertical(summary_dim_reduction, df)
+  return summary_dim_reduction, flag_first
+
+def get_summary_scaler(data_dic, regression=False, output=None):
+  scalers = list(data_dic.keys())
+  flag_first = True
+  summary_dim_reduction = None
+  for index in range(len(scalers)):
+    scaler = scalers[index]
+    summary_dim_reduction, flag_first = get_summary_algorithms(data_dic, scaler, flag_first, summary_dim_reduction, regression=regression, output=output)
+  columns = ["Tipo de Escalador", "Metodo", "No Componentes (98% Varianza)", "Error Cuadratico Medio Escalado"]
+  summary_dim_reduction = summary_dim_reduction[columns]
+  summary_dim_reduction["Numero de Componentes Optimo"] = summary_dim_reduction["No Componentes (98% Varianza)"]
+  summary_dim_reduction = summary_dim_reduction.drop("No Componentes (98% Varianza)", axis=1)
+  return summary_dim_reduction
+
+def executeRegressors(data_dic_pre, output_feature, name, sample=1):
+  metadata = {}
+  
+  scalers = list(data_dic_pre.keys())
+  algoritms = {
+    "pca": PCA_(),
+    "ica": ICA_()
+  }
+  linear_regressors = {
+    "regresion lineal": RawLinearRegression(),
+    "regresion robusta": RobustLinearRegression(),
+    "regresion stacking": stackingRegressor()
+  }
+  influencias = {
+    "": None,
+    "Con Influencias": LinearRegressionPreprocessing()
+  }
+  summary = None
+  flagSummary = True
+  for scaler in scalers:
+    for method in algoritms.keys():
+      for influencia in influencias.keys():
+        if influencia!="":
+          raw_data = influencias[influencia].get_influencias(
+            data_dic_pre[scaler],
+            output_feature
+          )
+        else:
+          raw_data = data_dic_pre[scaler]
+        for regressor in linear_regressors.keys():
+          method_ = algoritms[method]
+          regressor_ =  linear_regressors[regressor]
+          description = "scaler: {} - dim red: {} - regressor: {} - influencia: {}".format(scaler, method, regressor, influencia)
+          print(description)
+          method_.apply(raw_data.drop(output_feature, axis=1), scaler)
+          datos_reduce = pd.concat(
+            [
+              method_.metadata["transformed_data"],
+              raw_data[output_feature]
+            ],
+            axis=1
+          )
+          if regressor=="regresion stacking":
+            sample = 0.001
+          else:
+            sample = 1
+          metadata_ = regressor_.apply(
+              datos_reduce, 
+              output_feature, 
+              description=description, 
+              sample=sample
+            )
+          metadata[description] = metadata_["regressor"]
+          if flagSummary:
+            flagSummary = False
+            summary = metadata_["metric_information"]
+            summary.to_csv(name, index=False)
+            #mode='a'
+          else:
+            summary = metadata_["metric_information"]
+            summary.to_csv(name, index=False, mode='a', header=False)
+  return summary, metadata
+      
+
+# CHECKPOINT = False
+# output_feature = "Global_intensity"
+# executions = {
+#   "with Time": [CHECKPOINT, "withTime.csv", 1],
+#   "without Time": [CHECKPOINT, "withoutTime.csv", 1]
+# }
+
+# for execution in executions.keys():
+#   CHECKPOINT_LOF, name, sample = executions[execution]
+#   if execution == "with Time":
+#     data_imputed, data_dic_collection = executeOutlierAnalisis(CHECKPOINT_LOF)
+#   else:
+#     data_imputed, data_dic_collection = executeOutlierAnalisisWithoutTime(CHECKPOINT_LOF)
+#   data_dic = getDicScalerData(data_imputed, data_dic_collection)
+#   summary, metadata = executeRegressors(data_dic, output_feature, name=name, sample=sample)
